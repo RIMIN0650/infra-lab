34595d2120563f287a984c8ae83a50319468c618
kwon_pc <chris0540@naver.com>
2026-07-30 09:59:15 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/test/java/com/example/infralab/InfraLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infralab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
