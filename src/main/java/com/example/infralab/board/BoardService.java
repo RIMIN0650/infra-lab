ac1152b4f20f6fb31f212499a620142442bd49e5
kwon_pc <chris0540@naver.com>
2026-07-30 15:44:50 +0900

    [Feat] 게시글 작성 기능 구현

--- src/main/java/com/example/infralab/board/BoardService.java
+++ src/main/java/com/example/infralab/board/BoardService.java
@@ -1,5 +1,7 @@
 package com.example.infralab.board;
 
+import com.example.infralab.board.model.Board;
+import com.example.infralab.board.model.BoardDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,8 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
+    public boolean upload(BoardDto.BoardRegReq dto) {
+        Board board = boardRepository.save(BoardDto.BoardRegReq.toEntity(dto));
+        return board.getId() != null;
+    }
 }
--- src/main/java/com/example/infralab/board/model/BoardDto.java
+++ src/main/java/com/example/infralab/board/model/BoardDto.java
@@ -1,4 +1,26 @@
 package com.example.infralab.board.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class BoardDto {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BoardRegReq {
+        private String title;
+        private String contents;
+
+        public static Board toEntity(BoardRegReq dto) {
+            return Board.builder()
+                    .title(dto.title)
+                    .contents(dto.contents)
+                    .build();
+        }
+    }
+
 }
